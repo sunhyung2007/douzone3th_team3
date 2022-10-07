7c7c3999486ecfec63809693c259030cc9dc2c02
KOSA <KOSA@DESKTOP-RAN4J81>
2022-10-07 15:00:25 +0900

    ㄱㄱ

---
+++ baekjoon/baekjoon/src/baekjoon/DFSANDBFS.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class DFSANDBFS {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int Z = sc.nextInt();
+		
+		int[] a = new int[M*2];
+		for(int i = 0; i < M*2; i ++) {
+			a[i] = sc.nextInt();
+		}
+		
+		
+	}
+	
+	public void DFS(int a) {
+		
+	}
+}
